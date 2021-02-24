a8d4a05731e941d903e5aa132da4b6b4543cda82
jonupp <jonas.knupp@gmx.ch>
2021-02-24 16:51:16 +0100

    clean

--- test/someThing.js
+++
@@ -1,7 +0,0 @@
-import chai from "chai";
-const expect = chai.expect;
-describe("Test if it works", function () {
-    it("Should work", function () {
-        expect(true).to.be.true;
-    });
-});
