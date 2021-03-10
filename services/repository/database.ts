bc2e9db8c73f022e79b02272581379e07d4d0612
Joel Hirzel <joel.hirzel@icloud.com>
2021-03-10 16:33:27 +0100

    make mongodb connection reliable

--- services/repository/database.ts
+++ services/repository/database.ts
@@ -7,25 +7,44 @@ class Database {
     private db;
 
     constructor() {
-        setTimeout(() => {
-            MongoClient.connect(connectionURL, (err, client) => {
-                if (err) console.log("mongodb connection error: "+err);
-                this.db = client.db(dbName);
-                console.log("mongodb connection success");
-            });
-        }, 2000);
+        this.connectWithRetry();
+    }
+
+    connectWithRetry() {
+        MongoClient.connect(connectionURL, (err, client) => {
+            if (err) {
+                console.log("mongodb connection error: "+err);
+                console.log("mongodb connection RETRY");
+                setTimeout(() => this.connectWithRetry(), 1000);
+                return;
+            }
+            console.log("mongodb connection success");
+            this.db = client.db(dbName);
+        });
     }
 
     async insert(collection, entry) {
-        return await this.db.collection(collection).insertOne(entry);
+        if(this.db) {
+            return await this.db.collection(collection).insertOne(entry);
+        } else {
+            console.log("NOT YET CONNECTED TO DATABASE");
+        }
     }
 
     async update(collection, id, data) {
-        return await this.db.collection(collection).updateOne({_id: new ObjectID(id)}, {$set: data});
+        if(this.db) {
+            return await this.db.collection(collection).updateOne({_id: new ObjectID(id)}, {$set: data});
+        } else {
+            console.log("NOT YET CONNECTED TO DATABASE");
+        }
     }
 
     async get(collection, filter, sort, limit) {
-        return await this.db.collection(collection).find(filter).sort(sort).limit(limit).toArray();
+        if(this.db) {
+            return await this.db.collection(collection).find(filter).sort(sort).limit(limit).toArray();
+        } else {
+            console.log("NOT YET CONNECTED TO DATABASE");
+        }
     }
 }
 
